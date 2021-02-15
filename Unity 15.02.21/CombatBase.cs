1e084406f3e3389ec684ef1e400e4f2f6589d042
Baioretto <zuku005@gmail.com>
2021-02-15 19:30:58 +0100

    Create CombatBase.cs

---
+++ Unity 15.02.21/CombatBase.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatBase : MonoBehaviour
+{
+    public float fHealth=100;
+    public float fDamage=15;
+    
+    List<CombatBase> enemyList =new List<CombatBase>();
+    //var enemyList = new HashSet<CombatBase>(); 
+    void TryAttack(CombatBase enemy)
+    {
+        enemy.TakeDamage(fDamage);
+    }
+    void TakeDamage(float incDmg)
+    {
+        fHealth-=incDmg;
+    }
+    private void OnCollisionEnter(Collision other) {    
+        float fOponentDist;    
+        float fDistToFirstComparsion=1000.0f;
+        CombatBase cbEnemyToAttack;
+        enemyList.Add(other.gameObject.GetComponent<CombatBase>());  
+
+        foreach (var oponent in enemyList)
+        {
+            fOponentDist = Vector3.Distance(oponent.position, transform.position);
+            if(fOponentDist<distToFirstComparsion)
+            {
+                enemyToAttack=oponent;
+                distToFirstComparsion=fOponentDist;
+            }  
+        }  
+        if(enemyToAttack)
+                TryAttack(enemyToAttack);
+    }
+    private void OnCollisionExit(Collision other) {
+        foreach (var oponent in enemyList)
+        {
+            enemyList.Remove(other);
+        } 
+    }
+}
